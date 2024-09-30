da0752de25c8c49536f36d30a6859a1ba918abc8
serezha <medv420@ya.ru>
2024-09-30 18:48:36 +0500

    minute function added

--- main.cpp
+++ main.cpp
@@ -29,11 +29,27 @@ int get_mouth_day() // То же самое для дня месяца
     return day_of_month;
 }
 
+int get_minute() {
+    time_t now = time(0);
+    tm* local_time = localtime(&now);
+
+    int minute = local_time->tm_min;
+
+    //wcout << minute << endl;
+
+    if (minute % 2 == 0) {
+        //если минута четная
+    } else {
+        //если минута нечетная
+    }
+    return minute;
+}
 int main()
 {
     setlocale(LC_ALL, "Russian");
     cout<<get_week_day()<<endl;
     cout<<get_mouth_day()<<endl;
+    cout<<get_minute()<<endl;
     return 0;
 }
 
