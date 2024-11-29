7068f15043e338126a084eb0125962ab8ce5bde7
Veloner <savander122@gmail.com>
2024-11-29 10:52:59 +0500

    Add help for 1.6-1.8
    Add help for 1.6-1.8

--- help.h
+++ help.h
@@ -37,3 +37,62 @@ void help_1_5_9();
 void help_1_5_10();
 void help_1_5_11();
 void help_1_5_12();
+
+void help_1_6_1_1();
+void help_1_6_1_2();
+void help_1_6_1_3();
+void help_1_6_1_4();
+void help_1_6_1_5();
+void help_1_6_1_6();
+void help_1_6_1_7();
+void help_1_6_1_8();
+void help_1_6_1_9();
+void help_1_6_1_10();
+void help_1_6_1_11();
+void help_1_6_1_12();
+
+
+void help_1_6_2_1();
+void help_1_6_2_2();
+void help_1_6_2_3();
+void help_1_6_2_4();
+void help_1_6_2_5();
+void help_1_6_2_6();
+void help_1_6_2_7();
+void help_1_6_2_8();
+void help_1_6_2_9();
+void help_1_6_2_10();
+void help_1_6_2_11();
+void help_1_6_2_12();
+
+
+void help_1_7_1();
+void help_1_7_2();
+
+void help_1_8_1_1();
+void help_1_8_1_2();
+void help_1_8_1_3();
+void help_1_8_1_4();
+void help_1_8_1_5();
+void help_1_8_1_6();
+void help_1_8_1_7();
+void help_1_8_1_8();
+void help_1_8_1_9();
+void help_1_8_1_10();
+void help_1_8_1_11();
+void help_1_8_1_12();
+
+
+void help_1_8_2_1();
+void help_1_8_2_2();
+void help_1_8_2_3();
+void help_1_8_2_4();
+void help_1_8_2_5();
+void help_1_8_2_6();
+void help_1_8_2_7();
+void help_1_8_2_8();
+void help_1_8_2_9();
+void help_1_8_2_10();
+void help_1_8_2_11();
+void help_1_8_2_12();
+
